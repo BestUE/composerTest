5a5acb4386fb045722c4d925dd8bf9ba97a23d1e
kxy <kxy@xiaoshushidai.cn>
2019-06-04 16:05:07 +0800

    提交

---
+++ src/Hello.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xssd
+ * Date: 2019/6/4
+ * Time: 11:21
+ */
+
+namespace Hello;
+
+
+class Hello
+{
+    public static function world()
+    {
+        return 'Hello World!';
+    }
+}
---
+++ test.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xssd
+ * Date: 2019/6/4
+ * Time: 11:42
+ */
+
+require_once __DIR__ . '/vendor/autoload.php';
+use Hello\Hello;
+echo Hello::world();
